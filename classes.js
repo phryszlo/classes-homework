bb32b09c6c6479e0fa99e7c2ca53013f565a7416
phryszlo <phryszlo@gmail.com>
2022-08-26 09:32:02 -0400

    created timmys tale

--- classes.js
+++ classes.js
@@ -52,7 +52,7 @@ class Person {
     this.height++;
     this.weight++;
     this.mood--;
-    this.this.bankAccount += 10;
+    this.bankAccount += 10;
   }
   buyHamster(hamster) {
     this.hamsters.push(hamster);
@@ -65,4 +65,33 @@ const mickey = new Person("Mickey");
 mickey.buyHamster(jerry);
 console.log(mickey);
 
+const timmy = new Person("Timmy");
+//what's to stop me just doing timmy.age+=5?
+for (let i = 5; i > 0; i--) {
+  timmy.ageUp()
+}
+console.log(timmy.age);
+
+for (let i = 5; i > 0; i--) {
+  timmy.eat()
+}
+for (let i = 5; i > 0; i--) {
+  timmy.exercise()
+}
+for (let i = 9; i > 0; i--) {
+  timmy.ageUp()
+}
+const gus = new Hamster("Gus");
+timmy.buyHamster(gus);
+
+for (let i = 15; i > 0; i--) {
+  timmy.ageUp()
+}
+for (let i = 2; i > 0; i--) {
+  timmy.eat()
+}
+for (let i = 2; i > 0; i--) {
+  timmy.exercise()
+}
+console.log(timmy)
 
