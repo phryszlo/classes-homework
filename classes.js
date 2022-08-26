7c2f453d69bce059f1b3545b95960c791e5745f0
phryszlo <phryszlo@gmail.com>
2022-08-26 09:24:04 -0400

    created/tested Person class

--- classes.js
+++ classes.js
@@ -20,3 +20,49 @@ class Hamster {
 const jerry = new Hamster("Jerry");
 
 console.log(jerry);
+
+class Person {
+  age = 0;
+  height = 0;
+  weight = 0;
+  mood = 0;
+  hamsters = [];
+  bankAccount = 0;
+  constructor(name) {
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  eat() {
+    this.weight++;
+    this.mood++;
+  }
+  exercise() {
+    this.weight--;
+  }
+  ageUp() {
+    this.age++;
+    this.height++;
+    this.weight++;
+    this.mood--;
+    this.this.bankAccount += 10;
+  }
+  buyHamster(hamster) {
+    this.hamsters.push(hamster);
+    this.mood += 10;
+    this.bankAccount -= hamster.getPrice();
+  }
+}
+
+const mickey = new Person("Mickey");
+mickey.buyHamster(jerry);
+console.log(mickey);
+
+
